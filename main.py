5c9004a361789983c32b7b5ad11d81c15270d7c0
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-02-06 18:00:51 +0800

    fix bugs

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import subprocess
 import platform
 import os
-from src.language import zhcn
-from src.commit import *
+
+from src.commit import commit
 from src.settings import setting
 # ============================================ #
 language = setting.language
-# ============================================ #
 if language == 'zh-cn':
+    from src.language import zhcn
+# ============================================ #
+try:
     Caching_changes = zhcn.Caching_changes
     Caching_changes_finish = zhcn.Caching_changes_finish
     Please_select_the_type_of_changes_to_be_submitted = zhcn.Please_select_the_type_of_changes_to_be_submitted
     Please_enter_the_reasons_for_submission = zhcn.Please_enter_the_reasons_for_submission
+except ImportError:
+    pass
 # ============================================ #
 while True:
     print(Caching_changes)
@@ -24,7 +28,7 @@ while True:
         choose = input()
         if choose == '1':
             print(Please_enter_the_reasons_for_submission)
-            fix_bugs.fix_bugs()
+            commit.fix_bugs()
         commits = subprocess.Popen(r'src\commit\commit.sh',shell=True)
         commits.wait()
         push = subprocess.Popen(r'src\push\push.sh',shell=True)
