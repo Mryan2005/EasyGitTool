281554bfbba18a6f1ed5154a97e29845c160c092
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-02-06 08:45:24 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import subprocess
 import platform
 import os
+from src.push import push
+print(Caching_changes)
+add = subprocess.Popen(r'src\add\add.sh',shell=True)
+add.wait()
+print(Caching_changes_finish)
 if platform.system() == 'Windows':
     choose = input()
     if choose == '1':
-        update_type = 'fix bugs: '
-        reason = input('reason: ')
-        
+        push.fix_bugs()
+        push = subprocess.Popen(r'src\push\push.sh',shell=True)
+        push.wait()
--- src/push/push.py
+++ src/push/push.py
@@ -1,6 +1,8 @@
-reason = input()
-command = 'git commit -m fix bugs: '
-sh = open("push.sh", "w+")
-sh.write(command)
-sh.write(reason)
-sh.close()
+def fix_bugs():
+    reason = input()
+    command = "git commit -m 'fix bugs: "
+    sh = open(r"src\push\push.sh", "w+")
+    sh.write(command)
+    sh.write(reason)
+    sh.write("'")
+    sh.close()
