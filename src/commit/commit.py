746f75c9f1d021d665bfa179374e7c87c31dc298
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-02-07 07:39:59 +0800

    add update

--- src/commit/commit.py
+++ src/commit/commit.py
@@ -9,6 +9,16 @@ def fix_bugs():
     sh.write(reason)
     sh.write("'")
     sh.close()
-
+def update():
+    reason = input()
+    if reason == '':
+        command = "git commit -m 'update"
+    else:
+        command = "git commit -m 'update: "
+    sh = open(r"src\commit\commit.sh", "w+")
+    sh.write(command)
+    sh.write(reason)
+    sh.write("'")
+    sh.close()
 if __name__ == "__main__":
     fix_bugs()
