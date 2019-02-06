8c509f8aa828901e3ab930d439ab6d8268456979
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-02-07 07:43:02 +0800

    update: add exit

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import subprocess
 import os
-
 from src.commit import commit
 from src.settings import setting
+def main():
+    print(Please_select_the_type_of_changes_to_be_submitted)
+    print('1.fix bugs')
+    print('2.update')
+    print('3.exit')
+    choose = input()
+    if choose == '1':
+        print(Please_enter_the_reasons_for_submission)
+        commit.fix_bugs()
+        update()
+    elif choose == '2':
+        print(Please_enter_the_reasons_for_submission)
+        commit.update()
+        update()
+    elif choose == '3':
+        os._exit()
+    else:
+        main()
+# ============================================ #
+def update():
+    commits = subprocess.Popen(r'src\commit\commit.sh',shell=True)
+    commits.wait()
+    push = subprocess.Popen(r'src\push\push.sh',shell=True)
+    push.wait()
+    os.remove(r'src\commit\commit.sh')
+    main()
 # ============================================ #
 language = setting.language
 if language == 'zh-cn':
@@ -16,18 +41,8 @@ try:
 except ImportError:
     pass
 # ============================================ #
-while True:
-    print(Caching_changes)
-    add = subprocess.Popen(r'src\add\add.sh',shell=True)
-    add.wait()
-    print(Caching_changes_finish)
-    print(Please_select_the_type_of_changes_to_be_submitted)
-    print('1.fix bugs')
-    choose = input()
-    if choose == '1':
-        print(Please_enter_the_reasons_for_submission)
-        commit.fix_bugs()
-    commits = subprocess.Popen(r'src\commit\commit.sh',shell=True)
-    commits.wait()
-    push = subprocess.Popen(r'src\push\push.sh',shell=True)
-    push.wait()
+print(Caching_changes)
+add = subprocess.Popen(r'src\add\add.sh',shell=True)
+add.wait()
+print(Caching_changes_finish)
+main()
